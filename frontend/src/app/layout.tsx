785f4237ff0e8b32614e73375c2357a066141bfc
frulecalvi <frulecalvi@gmail.com>
2026-08-05 18:55:05 -0300

    Mejoras en favicon e imagen WA

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -6,7 +6,22 @@ import TopProgressBar from "@/components/TopProgressBar";
 
 export const metadata: Metadata = {
   title: "Parque de Descanso",
-  description: "Parque de Descanso - Cementerio privado en Mendoza",
+  description: "Empresa Fúnebre - Cementerio Parque - Cremaciones - Salas Velatorias",
+  icons: {
+    icon: "/assets/logo-icon-light-favicon.png",
+    shortcut: "/assets/logo-icon-light-favicon.png",
+    apple: "/assets/logo-icon-light-favicon.png",
+  },
+  openGraph: {
+    title: "Parque de Descanso",
+    description: "Empresa Fúnebre - Cementerio Parque - Cremaciones - Salas Velatorias",
+    images: [
+      {
+        url: "/assets/logo-full-whatsapp.png",
+        alt: "Parque de Descanso",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
